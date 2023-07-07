14073551740255b61aeada0d093053b0c5f4bcc0
tabascum <marques_vasco@yahoo.es>
2023-07-07 15:57:39 +0200

    Add mobile layout

--- main.js
+++ main.js
@@ -3,20 +3,14 @@ let i;
 
 for (i = 0; i < question.length; i++) {
   question[i].addEventListener("click", function () {
-    this.classList.add("active");
-
     let answer = this.nextElementSibling;
-    let arrow = this.querySelectorAll("#arrow")[0];
 
     if (answer.style.display === "block") {
       answer.style.display = "none";
-      arrow.classList.remove("arrow-rotate");
+      this.classList.remove("active");
     } else {
       answer.style.display = "block";
-      arrow.classList.add("arrow-rotate");
+      this.classList.add("active");
     }
   });
-
-  const allQuestion = document.querySelectorAll("#faq-question");
-  const allArrow = document.querySelectorAll("#arrow");
 }
